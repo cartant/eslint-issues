c043fbffcab4d2a946f9b494c43844284e7e9915
Nicholas Jamieson <nicholas@cartant.com>
2020-04-04 12:21:06 +1000

    chore: Add type assertions to break traversal.

--- src/eslint-plugin-rxjs/46/index.tsx
+++ src/eslint-plugin-rxjs/46/index.tsx
@@ -1,6 +1,12 @@
 import React, { FC } from "react";
-import { of } from "rxjs";
+import { Observable, of } from "rxjs";
 
-of(42).subscribe(async (value) => await value);
+// The problem is that the selectors for no-async-subscribe use 'has' and that
+// effects a traversal. Adding the following (unnecessary) type assertions will
+// break this repro - as their node types won't be present in the visitor keys.
+// It failure isn't the same as what Felix originally reported - it loops
+// forever - but this is the same as what he saw after upgrading esquery.
+
+(of(42 as number) as Observable<number>).subscribe(async (value) => await value);
 
 export const SomeComponent: FC<{}> = () => <span>some component</span>;
